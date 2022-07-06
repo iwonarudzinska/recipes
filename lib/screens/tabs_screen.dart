13be3409c8dff6627da95143e889068af6b30fca
Iwona Rudzinska <rudzinska.iwona96@gmail.com>
2022-07-06 11:37:33 +0200

    Add bottomNavigationBar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -10,6 +10,18 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, dynamic>> _pages = [
+    {'page': const CategoriesScreen(), 'title': 'Categories'},
+    {'page': const FavoritesScreen(), 'title': 'Your Favorite'},
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -17,30 +29,51 @@ class _TabsScreenState extends State<TabsScreen> {
       // initialIndex: 0,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Meals'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                ),
-                text: 'Favorites',
-              ),
-            ],
-          ),
+          title: Text(_pages[_selectedPageIndex]['title']),
+          // bottom: const TabBar(
+          //   tabs: [
+          //     Tab(
+          //       icon: Icon(
+          //         Icons.category,
+          //       ),
+          //       text: 'Categories',
+          //     ),
+          //     Tab(
+          //       icon: Icon(
+          //         Icons.star,
+          //       ),
+          //       text: 'Favorites',
+          //     ),
+          //   ],
+          // ),
         ),
-        body: const TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoritesScreen(),
+        body: _pages[_selectedPageIndex]['page'],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).colorScheme.secondary,
+          currentIndex: _selectedPageIndex,
+          type: BottomNavigationBarType.shifting,
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: const Icon(Icons.category),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: const Icon(Icons.star),
+              label: 'Favorites',
+            ),
           ],
         ),
+        // body: const TabBarView(
+        //   children: [
+        //     CategoriesScreen(),
+        //     FavoritesScreen(),
+        //   ],
+        // ),
       ),
     );
   }
