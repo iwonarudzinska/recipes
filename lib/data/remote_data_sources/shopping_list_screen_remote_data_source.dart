d6bb067e20e5362dd905a230f9b47e1334a2f44d
Iwona Rudzinska <rudzinska.iwona96@gmail.com>
2022-07-28 12:44:18 +0200

    Put items collection into separate user

--- lib/data/remote_data_sources/shopping_list_screen_remote_data_source.dart
+++ lib/data/remote_data_sources/shopping_list_screen_remote_data_source.dart
@@ -1,17 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ShoppingListScreenRemoteDataSource {
   Stream<QuerySnapshot<Map<String, dynamic>>?> shoppinglistRemoteData() {
-    return FirebaseFirestore.instance.collection('categories').snapshots();
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
+        .collection('categories')
+        .snapshots();
   }
 
   Future<void> add({required String title}) {
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
     return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
         .collection('categories')
         .add({'title': title});
   }
 
   Future<void> delete({required String id}) {
-    return FirebaseFirestore.instance.collection('categories').doc(id).delete();
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
+        .collection('categories')
+        .doc(id)
+        .delete();
   }
 }
